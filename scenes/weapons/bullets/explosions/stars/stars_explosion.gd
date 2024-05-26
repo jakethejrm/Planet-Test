bb77ad20b04612635a15ab915b456308812db043
Jacob <jakethejrm@gmail.com>
2024-05-26 16:54:36 -0700

    multiplayer stuff

--- scenes/weapons/bullets/explosions/stars/stars_explosion.gd
+++ scenes/weapons/bullets/explosions/stars/stars_explosion.gd
@@ -2,17 +2,12 @@ extends Node2D
 
 @export var lifetime : float = 1.0
 
-@onready var timer : Timer = $LifetimeTimer
 @onready var particles : GPUParticles2D = $Particles
 @onready var particles2 : GPUParticles2D = $Particles2
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	timer.start(lifetime)
-	pass # Replace with function body.
-
-func _on_lifetime_timer_timeout():
-	particles.emitting = false
-	particles2.emitting = false
+	particles.emitting = true
+	particles2.emitting = true
 	pass # Replace with function body.
 
 
